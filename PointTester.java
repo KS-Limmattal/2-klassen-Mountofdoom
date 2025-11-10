4fc82221256c847e0449987eddae247a872022ef
Yuanheng123123 <yuanheng.liu@stud.kslzh.ch>
2025-11-10 14:34:20 +0100

    swapped x and y coordinates

--- PointTester.java
+++ PointTester.java
@@ -4,6 +4,9 @@ public class PointTester{
     public static void main(String[] args) {
         Point p = new Point(2, 5);
         System.out.println(p.toString());
+        double x = p.getX();
+        double y = p.getY();
+        p.setLocation(y, x);
         Point q = new Point(p);
         System.out.println(q.toString());
     }
