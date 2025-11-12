61b7dbe2bb096e43e32952f0fbc6d3fa44777849
Yuanheng123123 <yuanheng.liu@stud.kslzh.ch>
2025-11-12 08:25:46 +0100

    added static variables, and fixed a bug with wrong assignement of lfraction

--- FractionTester.java
+++ FractionTester.java
@@ -1,36 +1,45 @@
 public class FractionTester {
     public static void main(String[] args) {
-        Fraction f = new Fraction(0, 3);
-        // Der folgende Code funktioniert nur, wenn die Felder von Fraction nicht privat sind:
+        Fraction f = new Fraction(0, 3); // create Fraction object
+        // Testcode of Fraction class:
+        // toString():
+        Fraction lFraction = null;
         
-        System.out.println(f.getNumerator() + "/" + f.getDenominator());
+        System.out.println(f.getNumerator() + "/" + f.getDenominator()); // print fraction
         try{
-        Fraction g = new Fraction(3, 0);
-        System.out.println(g.getNumerator() + "/" + g.getDenominator());
+            Fraction g = new Fraction(3, 0); // Fraction with denominator 0
+            System.out.println(g.getNumerator() + "/" + g.getDenominator());// print fraction
         
-        Fraction lFraction = new Fraction(0, 6);
+        
 
 
 
-        } catch (ArithmeticException e) {
+        } catch (ArithmeticException e) {   // Division by zero
             System.out.println("Fehler: " + e.getMessage());
         }
         Fraction h = new Fraction(f);
-        System.out.println(h.getNumerator() + "/" + h.getDenominator());
-        System.out.println(f.equals(h));
-        System.out.println(f.equals(lFraction));
+        // Encapsulation, Getter und Setter und Konstruktoren, equals():
+        System.out.println(h.getNumerator() + "/" + h.getDenominator());// print fraction
+        System.out.println(f.equals(h)); // should be true
+        lFraction = new Fraction(0, 6); // visible everywhere
+        System.out.println(f.equals(lFraction)); // should be true
        
         
-        // toString():
         
-        // Encapsulation, Getter und Setter:
 
-        // Copy-Konstruktor und equals():
         
-        // Klassen- und Instanzenmethoden:
+        
+        
 
         // statische Variablen:
+        //final double PI = 3.14159; konstante Variable
+        //try{
+       //     PI = 3.14; // Fehler: cannot assign a value to final variable PI
+        //}
+        //catch (Exception e){
+          //  System.out.println("Statische Variable: " + e.getMessage()); doesnt compile
 
-    }
+   // }
 
+}
 }
